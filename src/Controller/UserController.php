810d2f4c41ef0f1e39f600a1d5d91efa4f2fdcdd
masurejean <jean-philippe.masure@hotmail.com>
2022-10-17 14:38:05 +0200

    form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 
 class UserController extends AbstractController
 {
@@ -14,5 +20,15 @@ class UserController extends AbstractController
     public function index(): Response
     {
         return $this->render('user/index.html.twig');
+    }
+    /**
+     * @Route("/inscription", name="registration")
+     */
+    public function inscription(ManagerRegistry $doctrine, Request $resq,
+    UserPassportInterface $encoder, SluggerInterface $sluger){
+        $user = new User();
+        $from = $this->createForm(UserType::class, $user);
+        return $this ->render("user/add.html.twig",["formulaire" =>$from->createView()
+        ]);
     }
 }
